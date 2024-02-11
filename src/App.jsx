edacaaddf7ddf89795f231b74b903203ee624dfb
Antony Perez <varadme@yahoo.com>
2024-02-10 16:31:50 -0800

    JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
-
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Bird",
-      animal: "Pepper",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Cat",
-      animal: "Doink",
-      breed: "Mixed",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
